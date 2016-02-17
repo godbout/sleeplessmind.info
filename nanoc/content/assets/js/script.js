224894a193af61b39bd921bed5533feadcb9e8b4
Guill Lo <guill.bout@gmail.com>
2016-02-17 13:42:55 +0800

    Add a way to dynamically write years in the past without the ago suffix

--- nanoc/content/assets/js/script.js
+++ nanoc/content/assets/js/script.js
@@ -1,5 +1,9 @@
 /*global $, document */
 $(document).ready(function () {
     'use strict';
+
     $('time.timeago').timeago();
+    
+    jQuery.timeago.settings.strings.suffixAgo = '';
+    $('time.timeago-nosuffix').timeago();
 });
