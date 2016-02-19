998b96930ad4b3a701ab0320112f36fc248b4d6d
Guill Lo <guill.bout@gmail.com>
2016-02-19 20:49:43 +0800

    Update javascript, removing useless part of timeago-nosuffix

--- nanoc/content/assets/js/script.js
+++ nanoc/content/assets/js/script.js
@@ -2,8 +2,5 @@
 $(document).ready(function () {
     'use strict';
 
-    $('time.timeago').timeago();
-    
-    jQuery.timeago.settings.strings.suffixAgo = '';
-    $('time.timeago-nosuffix').timeago();
+	$('time.timeago').timeago();
 });
