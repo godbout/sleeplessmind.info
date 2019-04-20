2f309d514f9071b874bb682ef5d45e33a7fd3ff1
Guillaume Leclerc <guill.bout@gmail.com>
2019-04-21 00:54:03 +0800

    Move to semistandard js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,19 +1,19 @@
-let mix = require('laravel-mix')
-let build = require('./tasks/build.js')
+let mix = require('laravel-mix');
+let build = require('./tasks/build.js');
 
-mix.disableSuccessNotifications()
-mix.setPublicPath('source/assets/build')
+mix.disableSuccessNotifications();
+mix.setPublicPath('source/assets/build');
 mix.webpackConfig({
   plugins: [
     build.jigsaw,
     build.browserSync(),
     build.watch(['source/**/*.md', 'source/**/*.php', 'source/**/*.scss', '!source/**/_tmp/*'])
   ]
-})
+});
 
 mix.js('source/_assets/js/main.js', 'js')
   .extract(['timeago.js'])
   .postCss('source/_assets/css/main.css', 'css', [
     require('tailwindcss')
   ])
-  .version()
+  .version();
