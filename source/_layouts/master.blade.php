79ceda887c7c2ce146883655237ea88614d0d8d5
Guillaume Leclerc <guill.bout@gmail.com>
2019-04-28 17:01:57 +0800

    Fix favicon path

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -7,8 +7,8 @@
         <meta http-equiv="x-ua-compatible" content="ie=edge">
         <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
         <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicons/apple-touch-icon.png">
-        <link rel="icon" type="image/png" href="/assets/favicons//favicon-32x32.png" sizes="32x32">
-        <link rel="icon" type="image/png" href="/assets/favicons//favicon-16x16.png" sizes="16x16">
+        <link rel="icon" type="image/png" href="/assets/favicons/favicon-32x32.png" sizes="32x32">
+        <link rel="icon" type="image/png" href="/assets/favicons/favicon-16x16.png" sizes="16x16">
         <link rel="manifest" href="/assets/favicons/manifest.json">
         <link rel="mask-icon" href="/assets/favicons/safari-pinned-tab.svg" color="#000000">
     </head>
